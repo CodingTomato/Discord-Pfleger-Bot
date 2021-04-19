e617fc8433dba1e1f915b1806e31a0bc14b19875
Marco Taubert <mtaubert@neuro-net.eu>
2021-04-19 19:42:11 +0200

    Revert back to http

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const fs = require('fs')
-const https = require('https')
 const cors = require('cors');
 const app = express();
 
@@ -14,7 +12,7 @@ const port = process.env.API_PORT;
 const discordBot = require('./bot.js');
 const logService = require('./log.js');
 
-discordBot.start(process.env.STANDARD_PREFIX, process.env.DISCORD_BOT_TOKEN);
+discordBot.start(process.env.PREFIX, process.env.DISCORD_BOT_TOKEN);
 
 app.use(JSONbodyParser.urlencoded({ extended: false }));
 app.use(JSONbodyParser.json());
@@ -58,9 +56,6 @@ app.put('/api/addSilenceUser', function (req, res) {
   return res.send("Sent userid");
 });
 
-https.createServer({
-  key: fs.readFileSync('./ssl/server.key'),
-  cert: fs.readFileSync('./ssl/server.cert')
-}, app).listen(port, () => {
-  logService.log(`Pfleger Bot API listening on port ${port} (HTTPS)`)
+app.listen(port, () => {
+  logService.log(`Pfleger Bot API listening on port ${port}`);
 })
